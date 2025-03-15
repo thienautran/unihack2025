e4414a89aee53b746b5223d0acd024e57a3b40af
thienautran <thien.autran75@gmail.com>
2025-03-15 12:11:45 +1100

    feat: adds getGamesList and getGamePrompt

--- convex/games.ts
+++ convex/games.ts
@@ -1,9 +1,16 @@
+import { v } from 'convex/values';
 import { query } from './_generated/server';
 
-export const getPrompt = query({
+export const getGamesList = query({
   args: {},
   handler: async (ctx) => {
-    // return await ctx.db.query('games').collect();
-    return 'bruh';
+    return await ctx.db.query('games').collect();
+  },
+});
+
+export const getGamePrompt = query({
+  args: { gameId: v.id('games') },
+  handler: async (ctx, args) => {
+    return ctx.db.get(args.gameId);
   },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,26 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
-import { useQuery } from 'convex/react';
+import { useConvex, useQuery } from 'convex/react';
 import { api } from '../../convex/_generated/api';
 
 export default function Home() {
+  const convex = useConvex();
+  console.log(convex);
   console.log(process.env.NEXT_PUBLIC_CONVEX_URL);
-  const games = useQuery(api.games.getPrompt);
+  const games = useQuery(api.games.getGamesList);
+  const test = useQuery(api.games.getGamePrompt, {
+    gameId: 'j5703r868rbc0qgh9vw49z16m57c3bn2',
+  });
 
   console.log('games value: is ', games?.length);
   return (
     <div className='flex flex-1 flex-col gap-4 justify-center items-center h-screen bg-slate-950 text-white'>
       <h1 className='text-4xl'>Hello Unihack</h1>
-
+      <p>{test?.name}</p>
+      {games?.map((game, index) => {
+        return <p>{game.name}</p>;
+      })}
       <Sheet>
         <SheetTrigger asChild>
           <Button
